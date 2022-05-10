b81ea1386058890776fb982d7f5ebd130525afe4
Natalect <Natalect.org@protonmail.com>
2022-05-10 16:42:11 -0700

    whaa, nooo

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,19 +1,13 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import Providers from "next-auth/providers";
 
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
-        },
-      },
+    Providers.Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
+    // ...add more providers here
   ],
 });
