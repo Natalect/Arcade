629276955adf6786c15cde98150eef806811cc5a
Natalect <93022049+Natalect@users.noreply.github.com>
2022-05-11 09:25:21 -0700

    Update Login.js

--- components/Login.js
+++ components/Login.js
@@ -3,13 +3,8 @@ import { signIn } from "next-auth/client";
 function Login() {
   return (
     <div className="grid place-items-center">
-      <img
-        className="glogo"
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/500px-Google_2015_logo.svg.png"
-        alt=""
-      />
       <h1
-        className="p-5 bg-blue-500 rounded-full text-white text-center w-1/6 cursor-pointer"
+        className="login-header p-5 bg-blue-500 rounded-full text-white text-center w-1/6 cursor-pointer"
         onClick={signIn}
       >
         Login with Google
